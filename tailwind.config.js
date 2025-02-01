3d28bb0b9e11b2720b841de1a431ad484ee258e5
naiful.naife <naifulnaife009@gmail.com>
2025-02-01 21:25:27 +0600

    resposive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  darkMode: ["attribute", "data-theme"], 
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
